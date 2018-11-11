61114c4806e399b959604a4d3e5e34c5646101ce
pkuwwt <wwthunan@gmail.com>
2018-11-11 18:36:19 +0800

    Update sqlite_recipes.js

--- code/recipes/sqlite_recipes.js
+++ code/recipes/sqlite_recipes.js
@@ -1,8 +1,5 @@
-
 var sqlite3 = require('sqlite3');
 var Promise = require('bluebird');
-
-const open_db = file => new sqlite3.Database(file);
 
 const handle_error = (err,msg) => err ? Promise.reject(err.message) : Promise.resolve(msg);
 
@@ -10,11 +7,9 @@ const wrap_db_func = (funcname, msg) => (db, ...params) => new Promise((res,rej)
     db[funcname].apply(db, [...params, (err,arg) => handle_error(err, arg||msg).then(res).catch(rej)]); 
 });
 
-const close_db = wrap_db_func('close', 'close finished');
-
-const execute_sql_first = wrap_db_func('get');
-
-const execute_sql_all = wrap_db_func('all');
+const execute_sql_get_first = wrap_db_func('get');
+const execute_sql_get_all = wrap_db_func('all');
+const execute_sql_run = wrap_db_func('run');
 
 const column_to_spec = ({name,type,property}) => {
     if(!name) console.error('null name in column_spec');
@@ -33,12 +28,12 @@ const sql_create_table = (tbname, columns) => `CREATE TABLE IF NOT EXISTS ${tbna
 const sql_delete_table = tbname => `DROP TABLE IF EXISTS ${tbname};`;
 const sql_insert_row = (tbname, columns, row) => `INSERT INTO ${tbname} (${columns_names(columns)}) VALUES (${row_to_sql(row)});`;
 
-const has_table = (db, tbname) => execute_sql_first(db, sql_search_table(tbname));
-
-const create_table = (db,tbname,columns) => execute_sql_first(db, sql_create_table(tbname, columns));
-const delete_table = (db, tbname) => execute_sql_first(db, sql_delete_table(tbname));
-
-const insert_row = (db,tbname,columns,row) => execute_sql_first(db, sql_insert_row(tbname, columns, row));
+const open_db = file => new sqlite3.Database(file);
+const close_db = wrap_db_func('close', 'close finished');
+const has_table = (db, tbname) => execute_sql_get_first(db, sql_search_table(tbname));
+const create_table = (db,tbname,columns) => execute_sql_run(db, sql_create_table(tbname, columns));
+const delete_table = (db, tbname) => execute_sql_run(db, sql_delete_table(tbname));
+const insert_row = (db,tbname,columns,row) => execute_sql_run(db, sql_insert_row(tbname, columns, row));
 
 module.exports = {
     open_db,
