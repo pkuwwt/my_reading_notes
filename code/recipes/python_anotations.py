88cc99ccf52a40ade1b79c8bfbe3464126aa6d8b
pkuwwt <wwthunan@gmail.com>
2019-03-10 02:29:18 +0800

    Update python_anotations.py

--- code/recipes/python_anotations.py
+++ code/recipes/python_anotations.py
@@ -1,20 +1,24 @@
 
+def merge(d1, d2):
+    d = d1.copy()
+    d.update(d2)
+    return d
+
 # A decorator to inject variables
-def valueDecorator(*args, **kargs):
-    _args = args
-    _kargs = kargs
+def valueDecorator(*_args, **_kargs):
     def wrapper(f):
         def wrapper2(*args, **kargs):
-            wrapper2.__name__ = f.__name__
-            wrapper2.__doc__ = f.__doc__
-            nonlocal _args
-            nonlocal _kargs
-            wrapper2.Vars = wrapper2.__closure__[0].cell_contents
-            wrapper2.NamedVars = wrapper2.__closure__[1].cell_contents
             return f(*args, **kargs)
+        wrapper2.__name__ = f.__name__
+        wrapper2.__doc__ = f.__doc__
+        oldVars = getattr(f, 'Vars', [])
+        oldNamedVars = getattr(f, 'NamedVars', {})
+        wrapper2.Vars = oldVars + list(_args)
+        wrapper2.NamedVars = merge(oldNamedVars, _kargs)
         return wrapper2
     return wrapper
-    
+
+@valueDecorator(12, 13, a=2)
 @valueDecorator(10, 11, a=1)
 def func():
     print(func.Vars)
