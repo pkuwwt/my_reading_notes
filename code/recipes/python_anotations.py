97406f366621224fa2abb4af4704aef95ceec2a0
pkuwwt <wwthunan@gmail.com>
2019-03-10 01:22:26 +0800

    Create python_anotations.py

---
+++ code/recipes/python_anotations.py
@@ -0,0 +1,21 @@
+
+# A decorator to inject variables
+def valueDecorator(*args, **kargs):
+    _args = args
+    _kargs = kargs
+    def wrapper(f):
+        def wrapper2(*args, **kargs):
+            wrapper2.__name__ = f.__name__
+            wrapper2.__doc__ = f.__doc__
+            nonlocal _args
+            nonlocal _kargs
+            wrapper2.getVars = lambda: wrapper2.__closure__[0].cell_contents
+            wrapper2.getNamedVars = lambda: wrapper2.__closure__[1].cell_contents
+            return f(*args, **kargs)
+        return wrapper2
+    return wrapper
+    
+@valueDecorator(10, 11, a=1)
+def func():
+    print(func.getVars())
+    print(func.getNamedVars())
