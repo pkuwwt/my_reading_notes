76cefe1a8d58befd67128190d11e3c85699d92ab
pkuwwt <wwthunan@gmail.com>
2019-03-10 17:57:16 +0800

    Create yesod-hello.hs

---
+++ code/helloworld/yesod-hello.hs
@@ -0,0 +1,22 @@
+
+-- runhaskell yesod-hello.hs
+
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+import           Yesod
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
+main :: IO ()
+main = warp 3000 HelloWorld
